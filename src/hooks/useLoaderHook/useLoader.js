ea1a5cad1cbf101775bdcc9a9817acad645158e2
Chandra, Rajdeep <rajdeep.chandra@accenture.com>
2019-06-21 18:43:07 +0530

    eslint added

---
+++ src/hooks/useLoaderHook/useLoader.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+
+
+export const useLoader =  (isLoading) => {
+  return (
+      <div>
+          { isLoading ? 
+          <img src={window.location.origin + '/images/loaderhooks.gif'} alt="" />
+          : <div/> }
+      </div>
+  )
+}
